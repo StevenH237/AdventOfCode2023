363fb7fbbca8e401b03ea83ca25127f4b2e2004c
Steven <github@steven-h.net>
2023-12-02 17:39:40 -0500

    Completely overhaul repo with new template

--- src/Program.cs
+++ src/Program.cs
@@ -1,113 +1,108 @@
-public class AdventMain
+﻿using System.Reflection;
+
+public static class Program
 {
   static void Main(string[] args)
   {
-    var which = "";
+    string which = "";
 
     if (args.Any()) which = args.First();
     else
     {
-      Console.Write("Which program should be run? (\"All\", or a number [optionally followed by a or b]): ");
-      which = Console.ReadLine()?.ToLowerInvariant();
+      Console.Write("Which day's program should be run? (Enter a number): ");
+      which = Console.ReadLine().ToLowerInvariant();
     }
 
-    if (which == null)
+    Type dayType = Type.GetType($"Day{which}");
+
+    if (dayType == null)
     {
-      Console.WriteLine($"No value was specified. Exiting...");
+      Console.WriteLine("This day hasn't been written yet!");
+      return;
     }
-    else if (which == "all")
+
+    if (!Directory.Exists($"./data/day{which}"))
     {
-      foreach (string i in Enumerable.Range(1, 25).Select(x => x.ToString()))
+      Console.WriteLine("This day's data directory doesn't exist!");
+      return;
+    }
+
+    MethodInfo part1Method = dayType.GetMethod("Part1", new Type[] { typeof(StreamReader) });
+    if (part1Method != null)
+    {
+      Console.WriteLine("Running part 1...");
+      if (RunTests(which, 1, part1Method.CreateDelegate<Func<StreamReader, string>>()))
       {
-        Console.WriteLine($"Day {i}: {Programs[i]()}");
+        RunCode(which, 1, part1Method.CreateDelegate<Func<StreamReader, string>>());
       }
+      Console.WriteLine();
     }
-    else if (Programs.ContainsKey(which))
+
+    MethodInfo part2Method = dayType.GetMethod("Part2", new Type[] { typeof(StreamReader) });
+    if (part2Method != null)
     {
-      Console.WriteLine($"{which}: {Programs[which]()}");
-    }
-    else
-    {
-      Console.WriteLine($"The specified day does not exist: {which}");
+      Console.WriteLine("Running part 2...");
+      if (RunTests(which, 2, part2Method.CreateDelegate<Func<StreamReader, string>>()))
+      {
+        RunCode(which, 2, part2Method.CreateDelegate<Func<StreamReader, string>>());
+      }
+      Console.WriteLine();
     }
   }
 
-  static Dictionary<string, Func<string>> Programs = new()
+  static bool RunTests(string day, int part, Func<StreamReader, string> method)
   {
-    ["1"] = () => $"[PART 1] {Day1Main.Part1()} / [PART 2] {Day1Main.Part2()}",
-    ["1a"] = Day1Main.Part1,
-    ["1b"] = Day1Main.Part2,
-    ["2"] = () => $"[PART 1] {Day2Main.Part1()} / [PART 2] {Day2Main.Part2()}",
-    ["2a"] = Day2Main.Part1,
-    ["2b"] = Day2Main.Part2,
-    ["3"] = () => $"[PART 1] {Day3Main.Part1()} / [PART 2] {Day3Main.Part2()}",
-    ["3a"] = Day3Main.Part1,
-    ["3b"] = Day3Main.Part2,
-    ["4"] = () => $"[PART 1] {Day4Main.Part1()} / [PART 2] {Day4Main.Part2()}",
-    ["4a"] = Day4Main.Part1,
-    ["4b"] = Day4Main.Part2,
-    ["5"] = () => $"[PART 1] {Day5Main.Part1()} / [PART 2] {Day5Main.Part2()}",
-    ["5a"] = Day5Main.Part1,
-    ["5b"] = Day5Main.Part2,
-    ["6"] = () => $"[PART 1] {Day6Main.Part1()} / [PART 2] {Day6Main.Part2()}",
-    ["6a"] = Day6Main.Part1,
-    ["6b"] = Day6Main.Part2,
-    ["7"] = () => $"[PART 1] {Day7Main.Part1()} / [PART 2] {Day7Main.Part2()}",
-    ["7a"] = Day7Main.Part1,
-    ["7b"] = Day7Main.Part2,
-    ["8"] = () => $"[PART 1] {Day8Main.Part1()} / [PART 2] {Day8Main.Part2()}",
-    ["8a"] = Day8Main.Part1,
-    ["8b"] = Day8Main.Part2,
-    ["9"] = () => $"[PART 1] {Day9Main.Part1()} / [PART 2] {Day9Main.Part2()}",
-    ["9a"] = Day9Main.Part1,
-    ["9b"] = Day9Main.Part2,
-    ["10"] = () => $"[PART 1] {Day10Main.Part1()} / [PART 2] {Day10Main.Part2()}",
-    ["10a"] = Day10Main.Part1,
-    ["10b"] = Day10Main.Part2,
-    ["11"] = () => $"[PART 1] {Day11Main.Part1()} / [PART 2] {Day11Main.Part2()}",
-    ["11a"] = Day11Main.Part1,
-    ["11b"] = Day11Main.Part2,
-    ["12"] = () => $"[PART 1] {Day12Main.Part1()} / [PART 2] {Day12Main.Part2()}",
-    ["12a"] = Day12Main.Part1,
-    ["12b"] = Day12Main.Part2,
-    ["13"] = () => $"[PART 1] {Day13Main.Part1()} / [PART 2] {Day13Main.Part2()}",
-    ["13a"] = Day13Main.Part1,
-    ["13b"] = Day13Main.Part2,
-    ["14"] = () => $"[PART 1] {Day14Main.Part1()} / [PART 2] {Day14Main.Part2()}",
-    ["14a"] = Day14Main.Part1,
-    ["14b"] = Day14Main.Part2,
-    ["15"] = () => $"[PART 1] {Day15Main.Part1()} / [PART 2] {Day15Main.Part2()}",
-    ["15a"] = Day15Main.Part1,
-    ["15b"] = Day15Main.Part2,
-    ["16"] = () => $"[PART 1] {Day16Main.Part1()} / [PART 2] {Day16Main.Part2()}",
-    ["16a"] = Day16Main.Part1,
-    ["16b"] = Day16Main.Part2,
-    ["17"] = () => $"[PART 1] {Day17Main.Part1()} / [PART 2] {Day17Main.Part2()}",
-    ["17a"] = Day17Main.Part1,
-    ["17b"] = Day17Main.Part2,
-    ["18"] = () => $"[PART 1] {Day18Main.Part1()} / [PART 2] {Day18Main.Part2()}",
-    ["18a"] = Day18Main.Part1,
-    ["18b"] = Day18Main.Part2,
-    ["19"] = () => $"[PART 1] {Day19Main.Part1()} / [PART 2] {Day19Main.Part2()}",
-    ["19a"] = Day19Main.Part1,
-    ["19b"] = Day19Main.Part2,
-    ["20"] = () => $"[PART 1] {Day20Main.Part1()} / [PART 2] {Day20Main.Part2()}",
-    ["20a"] = Day20Main.Part1,
-    ["20b"] = Day20Main.Part2,
-    ["21"] = () => $"[PART 1] {Day21Main.Part1()} / [PART 2] {Day21Main.Part2()}",
-    ["21a"] = Day21Main.Part1,
-    ["21b"] = Day21Main.Part2,
-    ["22"] = () => $"[PART 1] {Day22Main.Part1()} / [PART 2] {Day22Main.Part2()}",
-    ["22a"] = Day22Main.Part1,
-    ["22b"] = Day22Main.Part2,
-    ["23"] = () => $"[PART 1] {Day23Main.Part1()} / [PART 2] {Day23Main.Part2()}",
-    ["23a"] = Day23Main.Part1,
-    ["23b"] = Day23Main.Part2,
-    ["24"] = () => $"[PART 1] {Day24Main.Part1()} / [PART 2] {Day24Main.Part2()}",
-    ["24a"] = Day24Main.Part1,
-    ["24b"] = Day24Main.Part2,
-    ["25"] = () => $"[PART 1] {Day25Main.Part1()} / [PART 2] {Day25Main.Part2()}",
-    ["25a"] = Day25Main.Part1,
-    ["25b"] = Day25Main.Part2
-  };
+    string[] examples = Directory.GetFiles($"data/day{day}", "example*.txt");
+    bool tested = false;
+
+    foreach (string fname in examples)
+    {
+      using StreamReader input = new(File.OpenRead(fname));
+
+      // The first two lines are the expected answers
+      string p1Answer = input.ReadLine();
+      string p2Answer = input.ReadLine();
+      string answer = (part == 1) ? p1Answer : p2Answer;
+
+      // A blank line means that this input file doesn't apply to this part
+      if (answer == "") continue;
+
+      // Now perform the test!
+      tested = true;
+      string given = method(input);
+
+      // Output the results!
+      Console.WriteLine($"Test file: {(new FileInfo(fname)).Name} / Expected output: {answer} / Actual output: {given}");
+
+      if (given != answer) return false;
+    }
+
+    if (!tested) Console.WriteLine("No test cases were provided (if this is part 2, did you remember to add answers?)! Continuing anyway...");
+    else Console.WriteLine("All tests passed!");
+
+    return true;
+  }
+
+  static void RunCode(string day, int part, Func<StreamReader, string> method)
+  {
+    using StreamReader input = new(File.OpenRead($"data/day{day}/input.txt"));
+
+    Console.WriteLine($"Result on puzzle input: {method(input)}");
+  }
+
+  // For convenience:
+  public static IEnumerable<string> GetLines(this StreamReader input)
+  {
+    for (string line = input.ReadLine(); line != null && line != ""; line = input.ReadLine())
+    {
+      yield return line;
+    }
+  }
+
+  public static string[] GetAllLines(this StreamReader input) => input.GetLines().ToArray();
+
+  public static string GetEverything(this StreamReader input)
+  {
+    return input.ReadToEnd();
+  }
 }
--- src/days/Day1.cs
+++ src/days/Day1.cs
@@ -1,36 +1,41 @@
 using System.Text.RegularExpressions;
 using Nixill.Utils;
 
-public static class Day1Main
+public static class Day1
 {
   static Regex firstDigitRegex = new(@"^\D*(\d)");
   static Regex lastDigitRegex = new(@"(\d)\D*$");
 
-  static Regex digits = new(@"(one|two|three|four|five|six|seven|eight|nine|\d)");
+  static Regex digits = new(@"(on(?=e)|tw(?=o)|thre(?=e)|fou(?=r)|fiv(?=e)|si(?=x)|seve(?=n)|eigh(?=t)|nin(?=e)|\d)");
 
-  static Dictionary<string, int> mappings = new() {
-    ["one"] = 1, ["1"] = 1,
-    ["two"] = 2, ["2"] = 2,
-    ["three"] = 3, ["3"] = 3,
-    ["four"] = 4, ["4"] = 4,
-    ["five"] = 5, ["5"] = 5,
-    ["six"] = 6, ["6"] = 6,
-    ["seven"] = 7, ["7"] = 7,
-    ["eight"] = 8, ["8"] = 8,
-    ["nine"] = 9, ["9"] = 9,
+  static Dictionary<string, int> mappings = new()
+  {
+    ["on"] = 1,
+    ["1"] = 1,
+    ["tw"] = 2,
+    ["2"] = 2,
+    ["thre"] = 3,
+    ["3"] = 3,
+    ["fou"] = 4,
+    ["4"] = 4,
+    ["fiv"] = 5,
+    ["5"] = 5,
+    ["si"] = 6,
+    ["6"] = 6,
+    ["seve"] = 7,
+    ["7"] = 7,
+    ["eigh"] = 8,
+    ["8"] = 8,
+    ["nin"] = 9,
+    ["9"] = 9,
     ["0"] = 0
   };
 
-  const string Filename = "data/day1" +
-    // "example2" +
-    ".txt";
-
-  public static string Part1()
+  public static string Part1(StreamReader input)
   {
-    var file = File.ReadAllLines(Filename);
     int sum = 0;
 
-    foreach (string line in file)
+    foreach (string line in input.GetLines())
     {
       Match mtc;
       int number = 0;
@@ -51,12 +56,11 @@ public static class Day1Main
     return sum.ToString();
   }
 
-  public static string Part2()
+  public static string Part2(StreamReader input)
   {
-    var file = File.ReadAllLines(Filename);
     int sum = 0;
 
-    foreach (string line in file)
+    foreach (string line in input.GetLines())
     {
       if (digits.TryMatch(line, out Match mtc))
       {
