b2cd40bc586275609941f0f82903fc228c0c1960
Steven <github@steven-h.net>
2023-12-08 15:15:11 -0500

    I think it's ready to test!

--- src/days/day8.cs
+++ src/days/day8.cs
@@ -2,7 +2,8 @@ using System.Text.RegularExpressions;
 
 public static class Day8
 {
-  static Regex nodeFormat = new(@"([A-Z]{3}) = ()"
+  static Regex nodeFormat = new(@"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)";
+  
   public static string Part1(string fname, StreamReader input)
   {
     Dictionary<string, D8Node> nodes;
@@ -11,8 +12,27 @@ public static class Day8
 
     foreach (string line in input.GetLines())
     {
-
+      Match mtc = nodeFormat.Match(line);
+      string nodeID = mtc.Groups[1].Value;
+      string nodeLN = mtc.Groups[2].Value;
+      string nodeRN = mtc.Groups[3].Value;
+      nodes[nodeID] = new D8Node() { Left = nodeLN, right = nodeRN };
     }
+    
+    int moves = 0;
+    D8Node startNode = nodes["AAA"];
+    
+    foreach (char move in steps.RepeatInfinite())
+    {
+      string moveTo = null;
+      if (move == 'L') moveTo = node.Left;
+      else moveTo = node.Right;
+      moves += 1;
+      if (moveTo == "ZZZ") return moves.ToString();
+      else node = nodes[MoveTo];
+    }
+    
+    throw new InvalidDataException("How did you reach this error?");
   }
 }
 
@@ -20,4 +40,29 @@ public struct D8Node
 {
   public string Left;
   public string Right;
+}
+
+public static class MoreExtensions
+{
+  public static IEnumerable<T> Repeat<T>(this IEnumerable<T> seq, int count)
+  {
+    foreach (int i in Enumerable.Range(0, count))
+    {
+      foreach (T item in seq)
+      {
+        yield return item;
+      }
+    }
+  }
+  
+  public static IEnumerable<T> RepeatInfinite<T>(this IEnumerable<T> seq)
+  {
+    while (true)
+    {
+      foreach (T item in seq)
+      {
+        yield return item;
+      }
+    }
+  }
 }
