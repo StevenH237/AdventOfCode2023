3e2472aa07da3c0af3fe8ee55e926eacd48f9dfe
StevenH237 <github@steven-h.net>
2023-12-08 19:23:36 +0000

    Did a little work

---
+++ src/days/day8.cs
@@ -0,0 +1,23 @@
+using System.Text.RegularExpressions;
+
+public static class Day8
+{
+  static Regex nodeFormat = new(@"([A-Z]{3}) = ()"
+  public static string Part1(string fname, StreamReader input)
+  {
+    Dictionary<string, D8Node> nodes;
+    string steps = input.ReadLine();
+    input.ReadLine();
+
+    foreach (string line in input.GetLines())
+    {
+
+    }
+  }
+}
+
+public struct D8Node
+{
+  public string Left;
+  public string Right;
+}
