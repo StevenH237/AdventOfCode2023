95e62bc463bd39b44491bff258a03fe417b104c1
Steven <github@steven-h.net>
2023-12-13 00:47:11 -0500

    Removing day 12 because I didn't solve it.
    Various branches have various attempts.

--- src/days/DayExtra12.cs
+++
@@ -1,8 +0,0 @@
-public class DayExtra12
-{
-  public static string Part1(string fname, StreamReader input)
-  {
-    Console.WriteLine(new D12Line("?###???????? 3,2,1").CountValidArrangements());
-    return "";
-  }
-}
