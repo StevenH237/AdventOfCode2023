c1b965c61efd055eed30d68f3d326f8cfba81238
Steven <github@steven-h.net>
2023-12-18 21:41:17 -0500

    Day 18 part 1

--- src/common/CommonObjects.cs
+++ src/common/CommonObjects.cs
@@ -1,5 +1,23 @@
 using System.Numerics;
 
+public struct Coordinate
+{
+  public int X;
+  public int Y;
+
+  public Coordinate(int x, int y)
+  {
+    X = x;
+    Y = y;
+  }
+
+  public static Coordinate FromRC(int r, int c) => new(c, r);
+  public static Coordinate FromXY(int x, int y) => new(x, y);
+
+  public static Coordinate Abs(Coordinate inp) => new(int.Abs(inp.X), int.Abs(inp.Y));
+  public static long DistanceSquared(Coordinate inp) => (long)inp.X * inp.X + (long)inp.Y * inp.Y;
+}
+
 public struct Direction : IEquatable<Direction>, IEqualityOperators<Direction, Direction, bool>, IUnaryNegationOperators<Direction, Direction>
 {
   public readonly Vector2 Normal;
@@ -18,12 +36,36 @@ public struct Direction : IEquatable<Direction>, IEqualityOperators<Direction, D
   public static readonly Direction Left = new(new(-1, 0));
   public static readonly Direction Right = new(new(1, 0));
 
+  static readonly Dictionary<Direction, Direction> RightTurns = new()
+  {
+    [Up] = Right,
+    [Right] = Down,
+    [Down] = Left,
+    [Left] = Up
+  };
+  public Direction TurnRight() => RightTurns[this];
+
+  static readonly Dictionary<Direction, Direction> LeftTurns = new()
+  {
+    [Up] = Left,
+    [Right] = Up,
+    [Down] = Right,
+    [Left] = Down
+  };
+  public Direction TurnLeft() => LeftTurns[this];
+
+  static readonly Dictionary<Direction, Direction> AroundTurns = new()
+  {
+    [Up] = Down,
+    [Right] = Left,
+    [Down] = Up,
+    [Left] = Right
+  };
+  public Direction TurnAround() => AroundTurns[this];
+
   public bool Equals(Direction other) => this.Normal == other.Normal;
 
-  public static Direction operator -(Direction value)
-  {
-    return new(-value.Normal);
-  }
+  public static Direction operator -(Direction value) => value.TurnAround();
 
   public static bool operator ==(Direction left, Direction right) => left.Equals(right);
   public static bool operator !=(Direction left, Direction right) => !left.Equals(right);
